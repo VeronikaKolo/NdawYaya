67ebf52205dfb8692094ad31d2786831742248e2
VeronikaKolo <57199243+VeronikaKolo@users.noreply.github.com>
2019-10-31 13:52:31 +0100

    Update robot.h

--- robot.h
+++ robot.h
@@ -9,6 +9,7 @@ public :
     position*positionRobot() const;
     bool detecterJoueur(const joueur&j)const;
     void deplacerRobot(const joueur&j);
+    void deplacerRobotAuHasard();
 
 private :
     position*d_case;
